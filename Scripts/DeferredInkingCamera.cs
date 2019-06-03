e9f4e2546577850d5e339d7eb4e69ee583c375bd
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2019-06-03 15:04:05 +0900

    Task #603 Render lines with C# scripts

---
+++ Scripts/DeferredInkingCamera.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace WCGL
+{
+    [ExecuteInEditMode]
+    public class DeferredInkingCamera : MonoBehaviour
+    {
+        static Material DrawMaterial;
+
+        Camera cam;
+        CommandBuffer commandBuffer;
+        RenderTexture renderTexture;
+
+        void Start() { } //for Inspector ON_OFF
+
+        void initRenderTexture()
+        {
+            renderTexture = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 16);
+            renderTexture.name = "DeferredInking";
+            renderTexture.antiAliasing = 4;
+        }
+
+        void Awake()
+        {
+            cam = GetComponent<Camera>();
+            if (cam == null)
+            {
+                Debug.LogError(name + " does not have camera.");
+                return;
+            }
+
+            commandBuffer = new CommandBuffer();
+            commandBuffer.name = "DeferredInking";
+            cam.AddCommandBuffer(CameraEvent.AfterSkybox, commandBuffer);
+
+            initRenderTexture();
+
+            if (DrawMaterial == null)
+            {
+                var shader = Shader.Find("Hidden/DeferredInking/Draw");
+                DrawMaterial = new Material(shader);
+            }
+        }
+
+        private void OnPreRender()
+        {
+            if (renderTexture.width != cam.pixelWidth || renderTexture.height != cam.pixelHeight)
+            {
+                renderTexture.Release();
+                initRenderTexture();
+            }
+
+            commandBuffer.SetRenderTarget(renderTexture);
+            commandBuffer.ClearRenderTarget(true, true, Color.clear);
+
+            foreach(var model in DeferredInkingModel.Instances)
+            {
+                if (model.isActiveAndEnabled == false) continue;
+
+                var lineMat = model.material;
+                if (lineMat == null) continue;
+
+                foreach(var mesh in model.meshes)
+                {
+                    if (mesh == null || mesh.enabled == false) continue;
+                    commandBuffer.DrawRenderer(mesh, lineMat);
+                }
+            }
+
+            commandBuffer.Blit(renderTexture, BuiltinRenderTextureType.CameraTarget, DrawMaterial);
+        }
+
+        private void OnPostRender()
+        {
+            commandBuffer.Clear();
+        }
+    }
+}
---
+++ Scripts/DeferredInkingModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WCGL
+{
+    [ExecuteInEditMode]
+    public class DeferredInkingModel : MonoBehaviour
+    {
+        public static List<DeferredInkingModel> Instances = new List<DeferredInkingModel>();
+
+        public Material material;
+        public List<Renderer> meshes = new List<Renderer>();
+
+        void Start() { } //for Inspector ON_OFF
+
+        void Awake()
+        {
+            Instances.Add(this);
+        }
+
+        void OnDestroy()
+        {
+            Instances.Remove(this);
+        }
+
+    }
+}
