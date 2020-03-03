6b0b9c2d245291195077c027227b98fa6ef625d4
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2020-03-04 01:26:58 +0900

    LineMeshクラスを別ファイルに分離

--- Scripts/DeferredInkingModel.cs
+++ Scripts/DeferredInkingModel.cs
@@ -1,72 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace WCGL
 {
     [ExecuteInEditMode]
     public class DeferredInkingModel : MonoBehaviour
     {
-        [System.Serializable]
-        public class Mesh
-        {
-            public Renderer mesh;
-            public Material material;
-            [Range(0, 255)] public int meshID;
-
-            private (Renderer original, UnityEngine.Mesh mesh, ComputeBuffer vertices) bakedMesh;
-
-            public void bakeMesh(CommandBuffer commandBuffer)
-            {
-                if (mesh == null) return;
-
-                var smr = mesh as SkinnedMeshRenderer;
-                if (smr != null)
-                {
-                    if (bakedMesh.mesh == null) bakedMesh.mesh = new UnityEngine.Mesh();
-                    smr.BakeMesh(bakedMesh.mesh);
-                }
-                else if(bakedMesh.original != mesh)
-                {
-                    bakedMesh.mesh = mesh.GetComponent<MeshFilter>().sharedMesh;
-                }
-
-                int len = bakedMesh.mesh.vertices.Length;
-                if (bakedMesh.vertices == null || bakedMesh.vertices.count != len)
-                {
-                    bakedMesh.vertices?.Release();
-                    bakedMesh.vertices = new ComputeBuffer(len, 12);
-                    bakedMesh.vertices.name = mesh.name;
-                }
-
-                if (smr != null || bakedMesh.original != mesh)
-                {
-                    bakedMesh.vertices.SetData(bakedMesh.mesh.vertices);
-                    bakedMesh.original = mesh;
-                }
-
-                commandBuffer.SetGlobalBuffer("_Vertices", bakedMesh.vertices);
-
-                var rb = smr?.rootBone;
-                if (rb == null) commandBuffer.SetGlobalMatrix("_RootBone", Matrix4x4.identity);
-                else
-                {
-                    var mat = rb.transform.worldToLocalMatrix * mesh.transform.localToWorldMatrix;
-                    commandBuffer.SetGlobalMatrix("_RootBone", mat);
-                }
-            }
-
-            ~Mesh()
-            {
-                bakedMesh.vertices?.Release();
-            }
-        }
-
         public static List<DeferredInkingModel> Instances = new List<DeferredInkingModel>();
 
         [Range(1, 255)] public int modelID = 255;
-        public List<Mesh> meshes = new List<Mesh>();
+        public List<LineMesh> meshes = new List<LineMesh>();
 
         void Start() { } //for Inspector ON_OFF
 
---
+++ Scripts/LineMesh.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace WCGL
+{
+    [System.Serializable]
+    public class LineMesh
+    {
+        public Renderer mesh;
+        public Material material;
+        [Range(0, 255)] public int meshID;
+
+        private (Renderer original, Mesh mesh, ComputeBuffer vertices) bakedMesh;
+
+        ~LineMesh()
+        {
+            bakedMesh.vertices?.Release();
+        }
+
+        public void bakeMesh(CommandBuffer commandBuffer)
+        {
+            if (mesh == null) return;
+
+            var smr = mesh as SkinnedMeshRenderer;
+            if (smr != null)
+            {
+                if (bakedMesh.mesh == null) bakedMesh.mesh = new Mesh();
+                smr.BakeMesh(bakedMesh.mesh);
+            }
+            else if (bakedMesh.original != mesh)
+            {
+                bakedMesh.mesh = mesh.GetComponent<MeshFilter>().sharedMesh;
+            }
+
+            int len = bakedMesh.mesh.vertices.Length;
+            if (bakedMesh.vertices == null || bakedMesh.vertices.count != len)
+            {
+                bakedMesh.vertices?.Release();
+                bakedMesh.vertices = new ComputeBuffer(len, 12);
+                bakedMesh.vertices.name = mesh.name;
+            }
+
+            if (smr != null || bakedMesh.original != mesh)
+            {
+                bakedMesh.vertices.SetData(bakedMesh.mesh.vertices);
+                bakedMesh.original = mesh;
+            }
+
+            commandBuffer.SetGlobalBuffer("_Vertices", bakedMesh.vertices);
+
+            var rb = smr?.rootBone;
+            if (rb == null) commandBuffer.SetGlobalMatrix("_RootBone", Matrix4x4.identity);
+            else
+            {
+                var mat = rb.transform.worldToLocalMatrix * mesh.transform.localToWorldMatrix;
+                commandBuffer.SetGlobalMatrix("_RootBone", mat);
+            }
+        }
+    }
+}
